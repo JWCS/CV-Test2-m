35911bd9b20992a4db039d3ec26a8e633e64aa62
JWCS <sauve031@umn.edu>
2016-03-06 01:16:29 -0600

    Friday's Work. Still not done

--- FilterMean.m
+++ FilterMean.m
@@ -19,3 +19,12 @@ function A = getXyAv(subMat)
     [X, Y, ~] = size(subMat);
     A = sum(sum(subMat, 1, 'native'), 2, 'native')/(X*Y);
 end
+function A = subMat(Mat, x, y, range)
+%subMat Pass in a matrix, coordinate, and range to take a submatrix of
+    %range must be odd, range is the side of square matrix subMat ...
+    %with coordinate x,y at the center; Mat must be 2D;
+    %given r = (range - 1)/2, x,y must be each r spaces or more from edge
+    r = (range - 1) / 2; %radius r
+    A(1:range, 1:range ) = ...
+        Mat( x - r : x + r, y - r : y + r, 1);
+end
--- Main.m
+++ Main.m
@@ -8,6 +8,7 @@ tic
 %filterScale = 3; %by 3 mean
 sobel = [1 2 1; 0 0 0; -1 -2 -1];
 side = 3;
+delIntenseFilter = 20;
 meanKernel = double( ones( side, side ) ) ...
     / double( side^2 ); % sidexside mean kernel
 Im = rot90( imread('H1.jpg'), -1 );
@@ -17,7 +18,7 @@ Im = rot90( imread('H1.jpg'), -1 );
  %   ImGrad( ImGrad ( Im ) ), meanKernel );%, sobel );
 Im3 = ImGrad( FilterMeanV2( double(rgb2gray(Im)), meanKernel ) );
 Im4 = uint8(Im3);%(1420:1480,940:1010));
-Im4(Im4<6) = 0;
+Im4( Im4 < delIntenseFilter ) = 0;
 image( Im4 )
 hold on
 %contour( Im3, 1 )
