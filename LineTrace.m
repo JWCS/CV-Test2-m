d5d13c0f4785e288bf63de015f92ed74f6956393
JWCS <sauve031@umn.edu>
2016-03-06 18:03:15 -0600

    Sunday's poorly timed work. Also, last commit was Saturday's work, not Friday's

--- LineTrace.m
+++ LineTrace.m
@@ -3,7 +3,7 @@ function [Lines, Lengths] = LineTrace( ReducedIm, maxDist )
 %   It would be dificult to have it only analyze the lines I want, and 
 %   then to follow them in the right direction. 
     [X, Y] = size( ReducedIm );%also assuming ReducedIm is uint8
-    seen = Logical(ReducedIm);
+    seen = uint8( ReducedIm > 0 );
     %p2 = ReducedIm(11,11);
     lineNum = 0;
     for x = 1:X
@@ -18,7 +18,7 @@ function [Lines, Lengths] = LineTrace( ReducedIm, maxDist )
                     v = p2 - p1;
                     p1 = p2;
                     C = Mat2Search( p2, v, maxDist, X, Y );
-                    [xN, yN, allZ] = nextHigh( C, p2, seen.*ReducedIm);
+                    [xN, yN, allZ] = nextHigh( C, p2, maxDist, seen.*ReducedIm);
                     p2 = [xN, yN];
                     seen( C(1):C(3), C(2):C(4) ) = 0; %Might be a better checkoff
                     if allZ || (p2(1)-p1(1)==0 && p2(2)-p1(2)==0)
@@ -37,20 +37,52 @@ function [Lines, Lengths] = LineTrace( ReducedIm, maxDist )
     end
     
 end
-function [ xN, yN, allZ ] = nextHigh( C, p2, Im )
+function [ xN, yN, allZ ] = nextHigh( C, p2, maxDist, Im )
 %nextHigh Returns xN, yN point of the highest ~= 0 point in the mat
-    x1 = C(1); y1 = C(2); x2 = C(3); y2 = C(4); struct = C(5); dir = C(6);
-    Im1Col = Im(x1:x2,y1:y2);
-    [ ~, I ] = max(Im1Col(:));
-    [ px, py ] = ind2sub( size( Im1Col ), I );
-    [xN, yN] = searchMat2Im(p2, px, py, struct, dir);
+    %C passes in this info from Mat2Search
+    x1 = uint8(C(1)); y1 = uint8(C(2)); x2 = uint8(C(3)); y2 = uint8(C(4)); 
+    struct = C(5); dir = C(6);
+    Im1Col = Im(x1:x2,y1:y2); %Sub mat to search of Im
+    [ ~, I ] = max(Im1Col(:)); %Indices fo the max of above
+    [ px, py ] = ind2sub( size( Im1Col ), I ); %Indices to subscripts
+    [xN, yN] = searchMat2Im(p2, px, py, maxDist, struct, dir);%To Coordinates
     if sum(sum(Im1Col),2) == 0
         allZ = true;
     else
         allZ = false;
     end
 end
-function [pX, pY] = searchMat2Im( p0, px, py, struct, dir)
-    
-    
+function [pX, pY] = searchMat2Im( p0, px, py, maxDist, struct, dir)
+    if struct == 1
+        if dir == 0
+            pX = p0(1) + px - 1;
+            pY = p0(2) + py - 1;
+        elseif dir == 1
+            pX = p0(1) + px - maxDist;
+            pY = p0(2) + py - 1;
+        elseif dir == 2
+            pX = p0(1) + px - maxDist;
+            pY = p0(2) + py - maxDist;
+        elseif dir == 3
+            pX = p0(1) + px - 1;
+            pY = p0(2) + py - maxDist;
+        end
+    elseif struct == 2
+        if dir == 0
+            pX = p0(1) + px - 1;
+            pY = p0(2) + py - maxDist/2;
+        elseif dir == 1
+            pX = p0(1) + px - maxDist/2;
+            pY = p0(2) + py - 1;
+        elseif dir == 2
+            pX = p0(1) + px - maxDist;
+            pY = p0(2) + py - maxDist/2;
+        elseif dir == 3
+            pX = p0(1) + px - maxDist/2;
+            pY = p0(2) + py - maxDist;
+        end
+    elseif struct == 3
+        pX = p0(1) + px - maxDist/2;
+        pY = p0(2) + py - maxDist/2;
+    end
 end
--- Main.m
+++ Main.m
@@ -8,7 +8,8 @@ tic
 %filterScale = 3; %by 3 mean
 sobel = [1 2 1; 0 0 0; -1 -2 -1];
 side = 3;
-delIntenseFilter = 20;
+deltaIntenseFilter = 20;
+maxDist = 10; %Even, max distance to search to next max
 meanKernel = double( ones( side, side ) ) ...
     / double( side^2 ); % sidexside mean kernel
 Im = rot90( imread('H1.jpg'), -1 );
@@ -18,7 +19,10 @@ Im = rot90( imread('H1.jpg'), -1 );
  %   ImGrad( ImGrad ( Im ) ), meanKernel );%, sobel );
 Im3 = ImGrad( FilterMeanV2( double(rgb2gray(Im)), meanKernel ) );
 Im4 = uint8(Im3);%(1420:1480,940:1010));
-Im4( Im4 < delIntenseFilter ) = 0;
+Im4( Im4 < deltaIntenseFilter ) = 0;
+[Lines, Lengths] = LineTrace( Im4, maxDist );
+[ ~, LongestLineNo ] = max(Lengths);
+LongestLineCoord = Lines( :, :, LongestLineNo ); %[x1, y1; x2, y2; x3, ...] 
 image( Im4 )
 hold on
 %contour( Im3, 1 )
